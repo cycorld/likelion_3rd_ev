3ce7aa14563f51a031417e87e5f57fcbf287b24c
cycorld <charleschoi87@gmail.com>
2015-08-01 14:30:12 +0900

    액션 없다는 에러가 떴으니 ev 액션 만들기

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -16,4 +16,7 @@ class HomeController < ApplicationController
     Tv.create(user_id: 1, title: params[:title], url: params[:url], intro: params[:intro])
     redirect_to '/'
   end
+
+  def ev
+  end
 end
